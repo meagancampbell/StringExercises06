023012a1fe6833742ed5d9d67960323725c21e2b
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-19 19:12:47 +0000

    working through the exercises again

--- index.js
+++ index.js
@@ -2,4 +2,16 @@
 
 JavaScript has no .title method, but that won't stop us!  Use the string methods you know to print 'Title Case' from 'title case'. */
 
+
+let language = 'JavaScript';
+console.log(language.slice(0,1)+language.slice(4,5));
+
+let initials = (language.slice(0,1)+language.slice(4,5));
+
+console.log(`The abbreviation for ${language} is ${initials}.`);
+
+
 let notTitle = 'title case';
+let titleCase = notTitle.replace('t', 'T').replace('c', 'C');
+console.log(titleCase);
+
